12039b9ede2cd710088778c386a7db69b62f4a41
Ethan Yap <ywj.ethan@gmail.com>
2026-01-20 18:12:26 +0800

    Fix visual select color

--- omarchy/.config/omarchy/current/theme/neovim.lua
+++ omarchy/.config/omarchy/current/theme/neovim.lua
@@ -2,6 +2,29 @@ return {
   {
     "bjarneo/ethereal.nvim",
     priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("ethereal")
+
+      local function set_custom_highlights()
+        -- Transparent floats
+        vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "SnacksNormalFloat", { bg = "NONE" })
+
+        -- Visual selection color
+        vim.api.nvim_set_hl(0, "Visual", { bg = "#24487d" })
+      end
+
+      set_custom_highlights()
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = set_custom_highlights,
+      })
+
+      vim.api.nvim_create_autocmd("UIEnter", {
+        callback = set_custom_highlights,
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
