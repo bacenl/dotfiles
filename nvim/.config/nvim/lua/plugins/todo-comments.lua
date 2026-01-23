6019e6dc084644c0841dd4f60f44c6209b4b561d
Ethan Yap <ywj.ethan@gmail.com>
2026-01-23 17:09:18 +0800

    Add todo-comments plugin

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  -- TODO: TODO:
+  -- NOTE: NOTE:
+  -- FIX: FIX:
+  -- WARNING: WARNING:
+  -- PERF: PERF:
+  -- HACK: HACK:
+
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
