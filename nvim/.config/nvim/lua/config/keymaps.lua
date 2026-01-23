68e236ad402400cd5ebcd7ad1fca4e940070a981
Ethan Yap <ywj.ethan@gmail.com>
2026-01-23 17:09:10 +0800

    Add todo.md and diary keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,19 @@
 -- To swap <C-f> to complete (from blink.cmp) with the buffer creation from neovim
 vim.keymap.set('c', '<C-y>', '<C-f>', { noremap = true, silent = true, desc = 'Open command-line window' })
 
+
+-- Keymap to open TODO
+local todo_path = '~/Documents/07_quick_notes/todo.md'
+
+vim.keymap.set('n', '<leader>td', function()
+  vim.cmd('edit ' .. todo_path)
+end, { desc = 'Open Todo' })
+
+-- Keymap to open programming diary
+local year = tonumber(os.date("%Y"))
+local month_abbr = os.date("%b"):lower()
+local diary_path = string.format("~/Documents/07_quick_notes/programming_diary/%d/%s.md", year, month_abbr)
+
+vim.keymap.set('n', '<leader>tp', function()
+  vim.cmd('edit ' .. diary_path)
+end, { desc = 'Open Programming Diary' })
