a73b0737850a0e62f0250f225a0de54195fe56a1
Ethan Yap <ywj.ethan@gmail.com>
2026-02-26 16:34:55 +0800

    Add keychain ssh to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path ~/.local/bin
 end
+if type -q keychain
+    SHELL=(which fish) keychain --quiet --eval id_ed25519 | source
+end
