0afa89d7d9257790909d5450fd6b9b4171e61920
Ethan Yap <ywj.ethan@gmail.com>
2026-02-26 16:34:46 +0800

    Update mkch directory

--- fish/.config/fish/functions/mkch.fish
+++ fish/.config/fish/functions/mkch.fish
@@ -1,5 +1,5 @@
 function mkch
-    set project_dir ~/Documents/03_projects/Chinese # Your project path
+    set project_dir ~/Documents/projects/chinese_md_to_anki/ # Your project path
     pushd $project_dir
     uv run python -m src.main $argv
     popd
