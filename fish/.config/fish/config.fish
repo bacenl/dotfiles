64a7782938e6777f96f5739baaf45db797093af8
Ethan Yap <ywj.ethan@gmail.com>
2026-06-02 11:20:42 +0800

    update fish for tmux and ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,5 +3,13 @@ if status is-interactive
     fish_add_path ~/.local/bin
 end
 
+if not set -q TMUX
+    tmux start-server
+    tmux new-session -d -s main 2>/dev/null
+    true
+end
+
 # SSH agent socket (managed by systemd)
-set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+end
